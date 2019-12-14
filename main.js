39b3826cfa253229636427c8cecad39fb20b6f83
Ben Cohen <benjcohen1985@gmail.com>
2019-12-14 15:08:03 -0500

    Apply Prettier formatting
    Display error message if input value is empty
    
    Display username on top of search results

--- main.js
+++ main.js
@@ -1,77 +1,73 @@
 function getUser() {
-    $('form').submit(function (event){
-        event.preventDefault();
-        getUrl();
-    });
+  const noInputError = "Please enter username";
+  $("form").submit(e => {
+    e.preventDefault();
+    if ($("input[type=text]").val() === "") {
+      $("#error-message").text(noInputError);
+      $("#user-info").empty();
+      return false;
+    }
+    getUrl();
+  });
 }
 
 function getUrl() {
-    let user = $('input').val();
-    let url ='https://api.github.com/users/:username/repos';
-    let array = url.split('/')
-    array[4] = user;
-    url = array.join('/');
-    getUserRepos(url);
-
+  let user = $("input").val();
+  let url = "https://api.github.com/users/:username/repos";
+  let array = url.split("/");
+  array[4] = user;
+  url = array.join("/");
+  getUserRepos(url);
 }
 function getUserRepos(url) {
-    fetch(url)
-        .then(response => {
-            if (response.ok) {
-                return response.json();
-            }
-            throw new Error(response.statusText);
-        })
-        .then(responseJson =>displayResults(responseJson))
-        .catch (err => {
-        $('#error-message').text(`Oops... ${err.message}`);
+  fetch(url)
+    .then(response => {
+      if (response.ok) {
+        return response.json();
+      }
+      throw new Error(response.statusText);
+    })
+    .then(responseJson => {
+      displayResults(responseJson);
+    })
+    .catch(() => {
+      $("#error-message").text(`Doggonit... Couldn't find user`);
 
-            $('input[type=text]').val("")
-
+      $("input[type=text]").val("");
     });
 }
 
-
-
-function displayResults(responseJson){
-    // iterate through the articles array, stopping at the max number of .repos
-
-    const data=responseJson;
-    for (let i = 0; i < data.length ; i++) {
-
-        $('.repos').append(
-            `<li><h3><a href=${data[i].html_url}>${data[i].name}</h3></a></li>`
-        )
-        clearContainer();
-        //clears input field
-        $('input[type=text]').val("")
-
-    };
-
+function displayResults(responseJson) {
+  // iterate through the articles array, stopping at the max number of .repos
+  const baseUrl = "https://github.com";
+  const data = responseJson;
+  for (let i = 0; i < data.length; i++) {
+    const repoName = data[i].name;
+    const repoLink = data[i].html_url;
+    const userName = data[i].owner.login;
+    const userHomePage = `${baseUrl}/${data[i].owner.login}`;
+    console.log(userHomePage);
+    $(".repos").append(`<li><h3><a href=${repoLink}>${repoName}</h3></a></li>`);
+    clearContainer();
+    //clears input field and any previous errors
+    $("input[type=text]").val("");
+    $("#error-message").empty();
+    // Dispaly username above list of repos
+    $("#user-info").html(
+      `<p>Here is a list of repos for user <a href="${userHomePage}"</a>${userName}</p>`
+    );
+  }
 }
-
-$(function () {
-    //ensures user can't click submit until input field is is cliked
-
-    $('input[type=text]').keyup(function () {
-        if ($(this).val() == '') {
-            $('.enableOnInput').prop('disabled', true);
-        } else {
-            $('.enableOnInput').prop('disabled', false);
-        }
-    });
-});
 
 function clearContainer() {
-    //clears image container for a new batch of pics
-    $('form').submit(function (event) {
-        $('.repos').empty();
-    });
-
+  //clears image container for a new batch of pics
+  $("form").submit(() => {
+    $(".repos").empty();
+  });
 }
 
 function renderPage() {
-    getUser();
+  getUser();
 }
 
-$(renderPage)
+$(renderPage);
